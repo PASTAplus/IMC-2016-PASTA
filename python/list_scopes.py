93be632c52f910ab259385b27140e862c475955e
Mark Servilla <mark.servilla@gmail.com>
2016-08-04 22:06:11 -0600

    Add list scopes script.

---
+++ python/list_scopes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: list_scopes
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    8/4/16
+"""
+
+import requests
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+    datefmt='%Y-%m-%d% H:%M:%S%z')
+logging.getLogger('').setLevel(logging.WARN)
+logger = logging.getLogger('list_scopes')
+
+
+
+def main():
+    scopes_url = 'https://pasta.lternet.edu/package/eml'
+    r = requests.get(scopes_url)
+
+    scopes = r.text.split('/n')
+    for scope in scopes:
+        print('{0}'.format(scope))
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
