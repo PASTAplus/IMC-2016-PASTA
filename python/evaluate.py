f16f5809086d7371a41bb6dd006d9f628bba2b72
Mark Servilla <mark.servilla@gmail.com>
2016-08-14 21:57:41 -0600

    Add evaluate data package script.

---
+++ python/evaluate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: evaluate
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    8/14/16
+"""
+
+import requests
+import sys
+from base64 import b64decode, b64encode
+
+
+def main(argv):
+
+#TODO write example for a data package evaluation using knb-lter-nin.1.1
+
+    return 0
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
